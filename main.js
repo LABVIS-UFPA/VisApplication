ffaddf0ffdda7508833b58b5a9a7c8d497493dbb
elvis thermo <elvisthermo@hotmail.com>
2020-01-30 11:35:58 -0300

    leitura json,tsv,e csv

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const Tray = electron.Tray;
 const path = require('path');
 const glob = require('glob');
 
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 const debug = /--debug/.test(process.argv[2]);
 let websocketmode = false;
 let websocketport = 0;
@@ -24,26 +24,26 @@ if (require.main !== module) {
     })
 }
 
-function initialize () {
+function initialize() {
     makeSingleInstance();
 
-    function createWindow () {
-        const appIcon = new Tray(__dirname+'/src/public/css/icons/home.png');
+    function createWindow() {
+        const appIcon = new Tray(__dirname + '/src/public/css/icons/home.png');
         const windowOptions = {
             width: 1080,
             minWidth: 680,
             height: 840,
             title: app.getName(),
-            icon:__dirname+'/src/public/css/icons/home.png'
+            icon: __dirname + '/src/public/css/icons/home.png'
         };
         // if (process.platform === 'linux') {
         //   windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
         // }
 
         ipcMain.on('not-data', (event, message) => {
-            if(message){
+            if (message) {
                 mainWindow.webContents.send('errror-data');
-            }else{
+            } else {
                 openFile();
             }
         });
@@ -60,9 +60,10 @@ function initialize () {
         const menuTemplateFile = {
             label: 'File',
             submenu: [
-                {label: 'load',click (){
+                {
+                    label: 'load', click() {
                         openFile();
-                }
+                    }
                 }]
         };
 
@@ -71,19 +72,19 @@ function initialize () {
             submenu: [
                 {
                     label: 'Select', submenu: [
-                        { label: 'Click Selection',click (){ } }
+                        { label: 'Click Selection', click() { } }
                     ]
                 },
                 {
                     label: 'Filter',
                     submenu: [
-                        { label: 'Filter By Data',click (){ } },
-                        { label: 'Filter Selected Data',click (){ } }
+                        { label: 'Filter By Data', click() { } },
+                        { label: 'Filter Selected Data', click() { } }
                     ]
                 },
-                { label: 'Zoom', click (){ } },
-                { label: 'Details on Demand',click (){ } },
-                { label: 'Sort',click (){ } },
+                { label: 'Zoom', click() { } },
+                { label: 'Details on Demand', click() { } },
+                { label: 'Sort', click() { } },
             ]
         };
 
@@ -93,7 +94,7 @@ function initialize () {
                 {
                     label: 'Toggle Developer Tools',
                     accelerator: process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
-                    click (item, focusedWindow) {
+                    click(item, focusedWindow) {
                         if (focusedWindow) focusedWindow.webContents.toggleDevTools()
                     }
                 }
@@ -106,8 +107,8 @@ function initialize () {
         mainWindow.setMenu(menu);
 
         //Work on Mac.
-        if(process.platform === 'darwin'){
-            mainWindow.on("focus", ()=>{
+        if (process.platform === 'darwin') {
+            mainWindow.on("focus", () => {
                 const menuTemplate = [menuTemplateFile, menuTemplateTools, menuTemplateDebug];
                 Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
             });
@@ -140,6 +141,8 @@ function initialize () {
     })
 }
 
+
+
 // Make this app a single instance app.
 //
 // The main window will be restored and focused instead of a second window
@@ -147,7 +150,7 @@ function initialize () {
 //
 // Returns true if the current version of the app should quit instead of
 // launching.
-function makeSingleInstance () {
+function makeSingleInstance() {
     if (process.mas) return;
 
     app.requestSingleInstanceLock();
@@ -160,29 +163,63 @@ function makeSingleInstance () {
     })
 }
 //open and convert datafile
-function openFile () {
+function openFile() {
     dialog.showOpenDialog(function (fileNames) {
         if (fileNames === undefined) return;
         let fileName = fileNames[0];
 
-        csv()
-            .fromFile(fileName)
-            .then((jsonObj)=>{
-                for(let i = 0; i < jsonObj.length; i++){
-                    let obj = jsonObj[i];
-                    for(let prop in obj){
-                        if(obj.hasOwnProperty(prop) && obj[prop] !== null && !isNaN(obj[prop])){
-                            obj[prop] = +obj[prop];
+        let checkType = fileName.split('\\');
+        checkType = checkType[checkType.length - 1].split('.');
+
+        if (checkType[1] == 'csv') {
+            console.log('csv');
+            csv()
+                .fromFile(fileName)
+                .then((jsonObj) => {
+                    for (let i = 0; i < jsonObj.length; i++) {
+                        let obj = jsonObj[i];
+                        for (let prop in obj) {
+                            if (obj.hasOwnProperty(prop) && obj[prop] !== null && !isNaN(obj[prop])) {
+                                obj[prop] = +obj[prop];
+                            }
                         }
                     }
-                }
-                mainWindow.webContents.send("file-data", jsonObj)
-            })
+                    console.log(jsonObj);
+                    mainWindow.webContents.send("file-data", jsonObj)
+                })
+        }
+        if (checkType[1] === 'json') {
+            let jsonObj = require(fileName);
+            mainWindow.webContents.send("file-data", jsonObj)
+        }
+
+        if(checkType[1]=='tsv' || checkType[1]=='txt'){
+            let jsonObj = fs.readFileSync(fileName, "utf8");
+            jsonObj = tsvJSON(jsonObj);
+            console.log(jsonObj);
+            mainWindow.webContents.send("file-data", jsonObj);
+
+        }
+    
+
     });
 }
 
+// tsv to Json convert
+function tsvJSON(tsv) {
+    const lines = tsv.split('\n');
+    const headers = lines.slice(0, 1)[0].split('\t');
+    return lines.slice(1, lines.length).map(line => {
+      const data = line.split('\t');
+      return headers.reduce((obj, nextKey, index) => {
+        obj[nextKey] = data[index];
+        return obj;
+      }, {});
+    });
+  }
+
 // Require each JS file in the main-process dir
-function loadDemos () {
+function loadDemos() {
     const files = glob.sync(path.join(__dirname, 'main-process/**/*.js'))
     files.forEach((file) => { require(file) })
 }
@@ -195,8 +232,8 @@ ipcMain.on('document-ready', function (evt, msg) {
     for (let arg of process.argv) {
         if (arg === 'websocketmode=on') {
             websocketmode = true
-        }else if (arg.startsWith('port=')) {
-            websocketport = +arg.replace("port=","");
+        } else if (arg.startsWith('port=')) {
+            websocketport = +arg.replace("port=", "");
         }
     }
 
@@ -207,7 +244,7 @@ ipcMain.on('document-ready', function (evt, msg) {
         const ws = new WebSocket(`ws://127.0.0.1:${websocketport}/`);
 
 
-        ws.on('open', function(){
+        ws.on('open', function () {
             mainWindow.webContents.send('cl', 'WS is open!');
             ws.send("oi");
         });
